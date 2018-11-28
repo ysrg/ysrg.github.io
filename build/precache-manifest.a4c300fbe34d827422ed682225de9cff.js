f5c675d2f539eb87edf21fe8f660b91698d9bac9
varxyz <mrsergiumoraru@gmail.com>
2018-11-28 23:21:39 +0200

    initial build commit

---
+++ build/precache-manifest.a4c300fbe34d827422ed682225de9cff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "05fe5d6d433b28de5f03",
+    "url": "/static/js/main.05fe5d6d.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "05fe5d6d433b28de5f03",
+    "url": "/static/css/main.1d56f544.chunk.css"
+  },
+  {
+    "revision": "1f9d4016465fdb4b7bc0675b5198f042",
+    "url": "/index.html"
+  }
+];
