2baf7ab349672da1db262377947e1cab4149d6c8
varxyz <mrsergiumoraru@gmail.com>
2018-11-29 17:59:00 +0200

    spinner loading until  images are dom ready

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Spinner from 'react-spinkit';
 import { FaMedium, FaGithub, FaEnvelope } from 'react-icons/fa';
 import logo from './logo.svg';
 import coinwatch from './img/coinwatch.png';
@@ -17,9 +18,13 @@ import heroku from './img/logo-heroku.svg';
 import jest from './img/logo-jest.svg';
 import graph from './img/logo-graph.svg';
 import './App.css';
+var OnImagesLoaded = require('react-on-images-loaded');
 
 class App extends Component {
+  state = { showImages: false };
   render() {
+    var hiddenStyle = { height: 0, overflow: 'hidden' };
+    var visibleStyle = {};
     return (
       <div className="App">
         <header>
@@ -48,70 +53,81 @@ class App extends Component {
             </a>
           </h2>
         </header>
-        <div className="content">
-          <div className="about">
-            Hi, I'm Sergey and I am a full-stack engineer. I love coding,
-            reading, running and physics. I like simple-to-read code that is
-            easy to reason about. Personal preferences are paradigms that
-            enforce and promote composability. I often ponder over matters such
-            as the approaching technological singularity or the probability that
-            time is just an illusion we use to measure the entropy of our
-            universe. My biggest strength is the ability to focus. I am the
-            antithesis of multitasking. If you would like to hire me or just say
-            hi, feel free to get in touch with me 👋.
-          </div>
-          <div className="stack">
-            <div class="strike">
-              <span>tech stack</span>
+        <div style={!this.state.showImages ? hiddenStyle : visibleStyle}>
+          <OnImagesLoaded
+            onLoaded={() => this.setState({ showImages: true })}
+            onTimeout={() => this.setState({ showImages: true })}
+            timeout={7000}
+          >
+            <div className="content">
+              <div className="about">
+                Hi, I'm Sergey and I am a full-stack engineer. I love coding,
+                reading, running and physics. I like simple-to-read code that is
+                easy to reason about. Personal preferences are paradigms that
+                enforce and promote composability. I often ponder over matters
+                such as the approaching technological singularity or the
+                probability that time is just an illusion we use to measure the
+                entropy of our universe. My biggest strength is the ability to
+                focus. I am the antithesis of multitasking. If you would like to
+                hire me or just say hi, feel free to get in touch with me 👋.
+              </div>
+              <div className="stack">
+                <div className="strike">
+                  <span>tech stack</span>
+                </div>
+                <div style={{ textAlign: 'center' }}>
+                  <img src={go} className="App-logo" alt="logo" />
+                  <img src={js} className="App-logo" alt="js" />
+                  <img src={node} className="App-logo" alt="logo" />
+                  <img src={aws} className="App-logo" alt="aws" />
+                  <img src={heroku} className="App-logo" alt="heroku" />
+                  <img src={firebase} className="App-logo" alt="firebase" />
+                  <img src={graph} className="App-logo" alt="graph" />
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <img src={redux} className="App-logo" alt="redux" />
+                  <img src={mongo} className="App-logo" alt="mongo" />
+                  <img src={babel} className="App-logo" alt="babel" />
+                  <img src={jest} className="App-logo" alt="jest" />
+                </div>
+              </div>
+              <div className="strike">
+                <span>my work</span>
+              </div>
+              <div className="gallery">
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://binancevol.herokuapp.com/"
+                >
+                  <img src={coinwatch} alt="coinwatch" />
+                </a>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://ichaty.herokuapp.com/"
+                >
+                  <img src={ichaty} alt="ichaty" />
+                </a>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://moonsky.herokuapp.com/"
+                >
+                  <img src={moviespot} alt="mobiespot" />
+                </a>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://ysrg.github.io/bookworm/"
+                >
+                  <img src={bookworm} alt="bookworm" />
+                </a>
+              </div>
             </div>
-            <div style={{ textAlign: 'center' }}>
-              <img src={go} className="App-logo" alt="logo" />
-              <img src={js} className="App-logo" alt="js" />
-              <img src={node} className="App-logo" alt="logo" />
-              <img src={aws} className="App-logo" alt="aws" />
-              <img src={heroku} className="App-logo" alt="heroku" />
-              <img src={firebase} className="App-logo" alt="firebase" />
-              <img src={graph} className="App-logo" alt="graph" />
-              <img src={logo} className="App-logo" alt="logo" />
-              <img src={redux} className="App-logo" alt="redux" />
-              <img src={mongo} className="App-logo" alt="mongo" />
-              <img src={babel} className="App-logo" alt="babel" />
-              <img src={jest} className="App-logo" alt="jest" />
-            </div>
-          </div>
-          <div class="strike">
-            <span>my work</span>
-          </div>
-          <div className="gallery">
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://binancevol.herokuapp.com/"
-            >
-              <img src={coinwatch} alt="coinwatch" />
-            </a>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://ichaty.herokuapp.com/"
-            >
-              <img src={ichaty} alt="ichaty" />
-            </a>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://moonsky.herokuapp.com/"
-            >
-              <img src={moviespot} alt="mobiespot" />
-            </a>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://ysrg.github.io/bookworm/"
-            >
-              <img src={bookworm} alt="bookworm" />
-            </a>
-          </div>
+          </OnImagesLoaded>
+        </div>
+        <div style={!this.state.showImages ? visibleStyle : hiddenStyle}>
+          <Spinner fadeIn="none" className="spinner" />
         </div>
       </div>
     );
