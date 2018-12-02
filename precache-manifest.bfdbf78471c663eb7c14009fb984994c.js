f00bacf309294646643d55c18ab6e4b929415c6f
varxyz <mrsergiumoraru@gmail.com>
2018-12-02 07:38:42 +0200

    removed background color

--- precache-manifest.bfdbf78471c663eb7c14009fb984994c.js
+++ precache-manifest.bfdbf78471c663eb7c14009fb984994c.js
@@ -4,8 +4,8 @@ self.__precacheManifest = [
     "url": "/static/media/logo-go.a52fa4f1.png"
   },
   {
-    "revision": "9ba10de9221a7c116d76",
-    "url": "/static/css/main.d47fd4fe.chunk.css"
+    "revision": "bdb3ea298bd47ea829a0",
+    "url": "/static/css/main.bdcfa76e.chunk.css"
   },
   {
     "revision": "5a4a847bcd232f4b3731bbfb9974f088",
@@ -44,8 +44,8 @@ self.__precacheManifest = [
     "url": "/static/media/node-logo.89ccb090.svg"
   },
   {
-    "revision": "9ba10de9221a7c116d76",
-    "url": "/static/js/main.9ba10de9.chunk.js"
+    "revision": "bdb3ea298bd47ea829a0",
+    "url": "/static/js/main.bdb3ea29.chunk.js"
   },
   {
     "revision": "dd60b11e9762b6e25f6d4ce981a1eeae",
@@ -84,7 +84,7 @@ self.__precacheManifest = [
     "url": "/static/css/1.d0014465.chunk.css"
   },
   {
-    "revision": "77635bb3b8631edde4e90cf9d4b6515f",
+    "revision": "3223c27e3c2d2d442a57d3bc21c45481",
     "url": "/index.html"
   }
 ];
