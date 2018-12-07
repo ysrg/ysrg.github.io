7e4c2e511117780da8d391436989d058444497b3
varxyz <mrsergiumoraru@gmail.com>
2018-12-07 17:06:59 +0200

    img perf improve

---
+++ precache-manifest.32c5ae97a4a8431ff1fc6281a09179e9.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "a52fa4f17f74cdef6a95b07adae8f87d",
+    "url": "/static/media/logo-go.a52fa4f1.png"
+  },
+  {
+    "revision": "cc9ab2a07f394b96392a",
+    "url": "/static/css/main.f3cdba50.chunk.css"
+  },
+  {
+    "revision": "5a4a847bcd232f4b3731bbfb9974f088",
+    "url": "/static/media/logo-graph.5a4a847b.svg"
+  },
+  {
+    "revision": "383135a198653a83ce0f",
+    "url": "/static/js/1.383135a1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "800f510861f63eb677f6b68622caae0a",
+    "url": "/static/media/coinwatch-sm.800f5108.png"
+  },
+  {
+    "revision": "3377cba4848c6dc05a42bf0441469dd5",
+    "url": "/static/media/ichaty-sm.3377cba4.png"
+  },
+  {
+    "revision": "fc4491be0265317971f5e45975c6069f",
+    "url": "/static/media/bookworm-sm.fc4491be.png"
+  },
+  {
+    "revision": "9c03cb81319454bde22f91516a041f9d",
+    "url": "/static/media/moviespot-sm.9c03cb81.png"
+  },
+  {
+    "revision": "89ccb09060a7adf788ea2340c54d6d78",
+    "url": "/static/media/node-logo.89ccb090.svg"
+  },
+  {
+    "revision": "cc9ab2a07f394b96392a",
+    "url": "/static/js/main.cc9ab2a0.chunk.js"
+  },
+  {
+    "revision": "dd60b11e9762b6e25f6d4ce981a1eeae",
+    "url": "/static/media/logo-js.dd60b11e.svg"
+  },
+  {
+    "revision": "259e1339a21cf9a16881f722296cb704",
+    "url": "/static/media/logo-aws.259e1339.svg"
+  },
+  {
+    "revision": "2aa7f7b569fd6e9176f49dd31eaa970e",
+    "url": "/static/media/logo-mongo.2aa7f7b5.svg"
+  },
+  {
+    "revision": "bfa69c4f3255d5c564c2ec600ff9885e",
+    "url": "/static/media/logo-firebase.bfa69c4f.svg"
+  },
+  {
+    "revision": "5ae1af16ffc2f55bd739da81176302e9",
+    "url": "/static/media/logo-redux.5ae1af16.svg"
+  },
+  {
+    "revision": "d255e75f6f38e21bc5b17f296248339c",
+    "url": "/static/media/logo-babel.d255e75f.svg"
+  },
+  {
+    "revision": "b38ff61aa327fbdc24f7b1dad7d04d1a",
+    "url": "/static/media/logo-heroku.b38ff61a.svg"
+  },
+  {
+    "revision": "ea9cdc83e9e0ebe92e5583f9ff466b5c",
+    "url": "/static/media/logo-jest.ea9cdc83.svg"
+  },
+  {
+    "revision": "383135a198653a83ce0f",
+    "url": "/static/css/1.d0014465.chunk.css"
+  },
+  {
+    "revision": "63d1d3d589439e25ab407a1108b2d84b",
+    "url": "/index.html"
+  }
+];
